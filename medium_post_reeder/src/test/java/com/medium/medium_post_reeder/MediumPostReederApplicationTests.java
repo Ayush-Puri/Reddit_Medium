f44686e53a30262d364d25d29219dea9cb260eef
Ayush Puri <ayushpuri.work@gmail.com>
2025-02-21 20:42:44 +0530

    Added Medium_Post_Reeder

---
+++ medium_post_reeder/src/test/java/com/medium/medium_post_reeder/MediumPostReederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medium.medium_post_reeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumPostReederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
